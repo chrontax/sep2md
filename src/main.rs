18345fbfa968a235aec43570b62f9576539d9dc1
chrontax <chrontax@proton.me>
2025-11-24 18:09:43 +0100

    whatever: add support for subarticles and improve error handling a little I guess
    I'm tired

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,31 @@ use std::{
 };
 
 use itertools::Itertools;
+use once_cell::sync::OnceCell;
 use scraper::{ElementRef, Html, Node, Selector};
+
+const SKIP_ELEMENTS: [&str; 1] = ["hr"];
+
+// I should do better than adding global state, but this app doesn't need exemplary code.
+static BASE_URL: OnceCell<String> = OnceCell::new();
 
 fn main() {
     let mut args = args();
-    let mut url = args.nth(1).expect("SEP page URL is required");
-    if !url.ends_with('/') {
-        url.push('/');
-    }
-    let outname = args
-        .next()
-        .unwrap_or_else(|| format!("{}.md", url.rsplit('/').nth(1).unwrap()));
+    let url = args.nth(1).expect("SEP page URL is required");
+    BASE_URL
+        .set(url.rsplit_once('/').unwrap().0.to_string())
+        .unwrap();
+    let outname = args.next().unwrap_or_else(|| {
+        format!(
+            "{}.md",
+            if url.ends_with('/') {
+                url[..url.len() - 1].rsplit_once('/').unwrap().1
+            } else {
+                let tmp = url.rsplit_once('/').unwrap().1;
+                tmp.rsplit_once('.').map(|(base, _)| base).unwrap_or(tmp)
+            }
+        )
+    });
     let html = reqwest::blocking::get(&url)
         .expect("Failed to request page HTML")
         .text()
@@ -29,6 +43,11 @@ fn main() {
     let title = document
         .select(&Selector::parse("h1").unwrap())
         .next()
+        .or_else(|| {
+            document
+                .select(&Selector::parse("#aueditable > h2").unwrap())
+                .next()
+        })
         .expect("Document lacks title")
         .text()
         .next()
@@ -42,15 +61,26 @@ fn main() {
     let mut main_els = document
         .select(&Selector::parse("#main-text").unwrap())
         .next()
+        .or_else(|| {
+            document // becuase why would subarticles have main-text 🙄
+                .select(&Selector::parse("#aueditable").unwrap())
+                .next()
+        })
         .expect("Document lacks main text")
         .child_elements()
         .peekable();
     let mut main_text = Vec::new();
     while main_els.peek().is_some() {
         main_text.push(Section::from_iter(&mut main_els));
+        while main_els
+            .peek()
+            .is_some_and(|el| SKIP_ELEMENTS.contains(&el.value().name()))
+        {
+            main_els.next().unwrap();
+        }
     }
 
-    let html = reqwest::blocking::get(format!("{}notes.html", url))
+    let html = reqwest::blocking::get(format!("{}/notes.html", url.rsplit_once('/').unwrap().0))
         .expect("Failed to request notes HTML")
         .text()
         .unwrap();
@@ -120,7 +150,7 @@ impl Article {
 
 #[derive(Debug)]
 struct Section {
-    id: String,
+    id: Option<String>,
     title: String,
     level: u8,
     text: Vec<Content>,
@@ -129,7 +159,7 @@ struct Section {
 impl Section {
     fn from_iter<'a, I: Iterator<Item = ElementRef<'a>>>(iter: &mut Peekable<I>) -> Self {
         let head = iter.next().unwrap();
-        let id = head.value().id().unwrap().to_string();
+        let id = head.value().id().map(String::from);
         let title = markdownify(head, &|_| false);
         let level = head.value().name()[1..].parse().unwrap();
         let mut text = Vec::new();
@@ -151,10 +181,12 @@ impl Section {
 
     fn markdown(self) -> String {
         format!(
-            "{} {} {{#{}}}\n\n{}",
+            "{} {}{}\n\n{}",
             "#".repeat(self.level as usize),
             self.title,
-            self.id,
+            self.id
+                .map(|id| format!(" {{#{}}}", id))
+                .unwrap_or_default(),
             self.text
                 .into_iter()
                 .map(Content::markdown)
@@ -169,6 +201,7 @@ enum Content {
     Blockquote(String),
     OrderedList(Vec<(Option<String>, String)>, usize),
     DefinitionList(Vec<(String, String)>),
+    Empty,
 }
 
 impl Content {
@@ -200,7 +233,17 @@ impl Content {
                     })
                     .collect(),
             ),
-            other => panic!("Unexpected content tag: {}", other),
+            "ul" => {
+                eprintln!(
+                    "Encountered a `ul`. Is this a table of contents?\n{}",
+                    el.html()
+                );
+                Self::Empty
+            }
+            other => {
+                eprintln!("Unexpected content tag: {}. Ignoring...", other);
+                Self::Empty
+            }
         }
     }
 
@@ -226,6 +269,7 @@ impl Content {
                 .iter()
                 .map(|(dt, dd)| format!("{}\n\n: {}\n\n", dt, dd))
                 .collect::<String>(),
+            Self::Empty => String::new(),
         }
     }
 }
@@ -248,7 +292,11 @@ fn markdownify<F: Fn(&Node) -> bool>(el: ElementRef<'_>, ignore: &F) -> String {
                     result.push('*');
                 }
                 "a" => {
-                    let href = el.attr("href").unwrap();
+                    let mut href = el.attr("href").unwrap().to_string();
+                    if !href.contains('/') && !href.contains('#') {
+                        // probably a link to a subarticle
+                        href = format!("{}/{}", BASE_URL.get().unwrap(), href);
+                    }
                     result += &format!(
                         "[{}]({})",
                         ElementRef::wrap(child).unwrap().text().next().unwrap(),
